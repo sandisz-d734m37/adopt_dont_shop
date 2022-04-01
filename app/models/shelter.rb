e7ec150a053eab0484ccc65c6cbe4179bcb91a90
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-01 00:28:48 -0600

    Give Shelter model ability to sort shelters in reverse alphabetical order

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -16,6 +16,10 @@ class Shelter < ApplicationRecord
       .order("pets_count DESC")
   end
 
+  def self.reverse_alpha
+    Shelter.find_by_sql("SELECT name FROM shelters ORDER BY name desc")
+  end
+  
   def pet_count
     pets.count
   end
