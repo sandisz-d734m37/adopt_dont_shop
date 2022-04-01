8ac2f41d479a6256a1e40f261d1f131f4fe31dba
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-01 15:18:10 -0600

    Test: Admin/Shelter index lists shelters w pending apps alphabetically

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,7 +5,10 @@ describe 'admin shelter index page' do
     @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    @shelter_4 = Shelter.create!(name: 'AAA Shelter', city: 'Denver, CO', foster_program: true, rank: 10)
 
+    @moody = @shelter_2.pets.create!(name: 'Moody', breed: 'unidentified', age: 3, adoptable: true)
+    @dilly = @shelter_4.pets.create!(name: 'Dilly', breed: 'unidentified', age: 3, adoptable: true)
     @pirate = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
     @clawdia = @shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
     @lucille = @shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
@@ -28,9 +31,29 @@ describe 'admin shelter index page' do
       description: 'Awaiting Information...',
       status: 'Rejected'
     )
+    @appl_3 = Application.create!(
+      name: 'Doomy',
+      street_address: '123 everywhere',
+      city: 'NotHereTown',
+      state: 'TH',
+      zip_code: 12345,
+      description: 'Awaiting Information...',
+      status: 'Pending'
+    )
+    @appl_4 = Application.create!(
+      name: 'Liddy',
+      street_address: '1456 everywhere',
+      city: 'HereHereTown',
+      state: 'TH',
+      zip_code: 12345,
+      description: 'Awaiting Information...',
+      status: 'Pending'
+    )
 
+    PetApplication.create!(pet_id: @moody.id, application_id: @appl_3.id)
     PetApplication.create!(pet_id: @pirate.id, application_id: @appl_1.id)
     PetApplication.create!(pet_id: @lucille.id, application_id: @appl_2.id)
+    PetApplication.create!(pet_id: @dilly.id, application_id: @appl_4.id)
 
     visit '/admin/shelters'
   end
@@ -44,6 +67,15 @@ describe 'admin shelter index page' do
     expect(page).to have_content("Shelter's with Pending Applications")
     within '#pending_shelters' do
       expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+      expect(page).not_to have_content(@shelter_3.name)
+    end
+  end
+
+  it 'lists shelters with pending applications in alphabetical order' do
+    within '#pending_shelters' do
+      expect(@shelter_4.name).to appear_before(@shelter_1.name)
+      expect(@shelter_1.name).to appear_before(@shelter_2.name)
       expect(page).not_to have_content(@shelter_3.name)
     end
   end
