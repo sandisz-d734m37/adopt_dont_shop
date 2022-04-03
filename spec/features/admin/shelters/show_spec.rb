f256c7b67da57c81749927cf5b4985dad6ae79d6
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-02 19:35:41 -0600

    Teat: Shelter Show page shows their number of adopted pets

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -6,9 +6,9 @@ describe 'admin shelter index page' do
     @shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-    @pirate = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @pirate = @shelter_1.pets.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
     @clawdia = @shelter_1.pets.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @hariett = @shelter_1.pets.create!(name: 'Hariett', breed: 'Cat', age: 10, adoptable: false)
+    @hariett = @shelter_1.pets.create!(name: 'Hariett', breed: 'Cat', age: 10, adoptable: true)
     @lucille = @shelter_3.pets.create!(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
 
     @appl_1 = Application.create!(
@@ -18,7 +18,7 @@ describe 'admin shelter index page' do
       state: 'TH',
       zip_code: 99999,
       description: 'Awaiting Information...',
-      status: 'Pending'
+      status: 'Approved'
     )
     @appl_2 = Application.create!(
       name: 'Johnny',
@@ -30,7 +30,7 @@ describe 'admin shelter index page' do
       status: 'Rejected'
     )
 
-    PetApplication.create!(pet_id: @pirate.id, application_id: @appl_1.id)
+    PetApplication.create!(pet_id: @pirate.id, application_id: @appl_1.id, pet_app_status: true)
     PetApplication.create!(pet_id: @lucille.id, application_id: @appl_2.id)
 
     visit "/admin/shelters/#{@shelter_1.id}"
@@ -52,7 +52,13 @@ describe 'admin shelter index page' do
 
   it 'shows the average age of all adoptable pets' do
     within '#statistics' do
-      expect(page).to have_content("Average adoptable pets age: 4.0")
+      expect(page).to have_content("Average adoptable pets age: 6.5")
+    end
+  end
+
+  it 'shows the number of pets that have been adopted' do
+    within '#statistics' do
+      expect(page).to have_content("Adopted pets: 1")
     end
   end
 end
