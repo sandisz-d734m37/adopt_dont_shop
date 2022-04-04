53187deea29cd7ad861743731c009589356e8a6a
Alex Psitos <apsitos08@gmail.com>
2022-04-04 18:29:33 -0400

    Test: show page displays correct fields before and after adding pets and description

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'the application show page' do
     expect(page).to have_content(application_4.city)
     expect(page).to have_content(application_4.zip_code)
     expect(page).to have_content(application_4.state)
-    expect(page).to have_content(application_4.status)
+    # expect(page).to have_content(application_4.status)
     # end
   end
 
@@ -46,7 +46,7 @@ RSpec.describe 'the application show page' do
 
     ozzy = furry_friends.pets.create!(name: "Ozzy", age: 1, breed: "dog", adoptable: true)
 
-    application_4 = Application.create!(name: "Marky Mark", street_address: "678 I Way", city: "Richmond", zip_code: 23229, state: "VA", description: "Awaiting Information", status: "In progress")
+    application_4 = Application.create!(name: "Marky Mark", street_address: "678 I Way", city: "Richmond", zip_code: 23229, state: "VA", description: "", status: "In progress")
 
     pet_application_5 = PetApplication.create!(pet_id: olive.id, application_id: application_4.id)
 
@@ -62,7 +62,12 @@ RSpec.describe 'the application show page' do
   end
 
   it 'does not render the search bar if application is not in progress' do
-    application_3 = Application.create!(name: "Steve Jobs", street_address: "456 I Way", city: "Richmond", zip_code: 23229, state: "VA", description: "Awaiting Information", status: "Rejected")
+    application_3 = Application.create!(name: "Steve Jobs", street_address: "456 I Way", city: "Richmond", zip_code: 23229, state: "VA", description: "Awaiting information", status: "Rejected")
+    furry_friends = Shelter.create!(name: "Furry Friends", foster_program: true, city: "Denver", rank: "2")
+
+    olive = furry_friends.pets.create!(name: "Olive", age: 2, breed: "dog", adoptable: true)
+    pet_application_6 = PetApplication.create!(pet_id: olive.id, application_id: application_3.id, pet_app_status: false )
+
 
     visit "/applications/#{application_3.id}"
 
@@ -96,7 +101,9 @@ RSpec.describe 'the application show page' do
 
     ozzy = furry_friends.pets.create!(name: "Ozzy", age: 1, breed: "dog", adoptable: true)
 
-    application_4 = Application.create!(name: "Marky Mark", street_address: "678 I Way", city: "Richmond", zip_code: 23229, state: "VA", description: "Awaiting Information", status: "In progress")
+    application_4 = Application.create!(name: "Marky Mark", street_address: "678 I Way", city: "Richmond", zip_code: 23229, state: "VA", description: "Awaiting information", status: "In progress")
+
+    # pet_application_5 = PetApplication.create!(pet_id: olive.id, application_id: application_4.id)
 
     visit "/applications/#{application_4.id}"
     expect(page).to_not have_content("Why I would be a good pet owner")
@@ -106,10 +113,11 @@ RSpec.describe 'the application show page' do
     click_button "Adopt this Pet"
     expect(page).to have_content("Why I would be a good pet owner")
     expect(page).to have_button("Submit my application")
-    fill_in('Description', with: 'I just love petting dogs, fam')
+    fill_in('description', with: 'I just love petting dogs, fam')
+
     click_button "Submit my application"
-    expect(page).to have_content("Status: Pending")
-    expect(page).to_not have_content("Status: In progress")
+    expect(page).to have_content("Status : Pending")
+    expect(page).to_not have_content("Status : In progress")
     expect(page).to_not have_button("Search")
     expect(page).to_not have_button("Submit my application")
   end
