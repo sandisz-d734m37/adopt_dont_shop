585f5a9fd4aac8548032657b138e1ebd0b29ddca
Alex Psitos <apsitos08@gmail.com>
2022-04-04 13:29:35 -0400

    Attempt to get procfile working for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   get '/applications/new', to: 'applications#new'
   get '/applications/:id', to: 'applications#show'
-  post '/applications/:id', to: 'applications#create'
+  post '/applications/', to: 'applications#create'
   get '/applications/:id/edit', to: 'applications#edit'
   patch '/applications/:id', to: 'applications#update'
   delete '/applications/:id', to: 'applications#destroy'
