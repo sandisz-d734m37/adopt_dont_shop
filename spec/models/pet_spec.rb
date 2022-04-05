0ac1312a88934155f4b25a52772113b4d898fcbc
Alex Psitos <apsitos08@gmail.com>
2022-04-05 13:07:40 -0400

    Partial search passes

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -23,6 +23,10 @@ RSpec.describe Pet, type: :model do
     describe '#search' do
       it 'return partial matches' do
         expect(Pet.search("Claw")).to eq([@pet_2])
+        expect(Pet.search("Pira")).to eq([@pet_1])
+      end
+      it 'case insensitive' do
+        expect(Pet.search("claw")).to eq([@pet_2])
       end
     end
 
