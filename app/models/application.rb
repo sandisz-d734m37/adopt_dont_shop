238ea0da356beb9a96d0b69c7cac20a8031a507f
Alex Psitos <apsitos08@gmail.com>
2022-04-05 15:01:58 -0400

    refactor update pet adoptability and application approval

--- app/models/application.rb
+++ app/models/application.rb
@@ -15,19 +15,19 @@ class Application < ApplicationRecord
   has_many :shelters, through: :pets
 
 
-  def self.pets_in_application(appl_id)
-    select("applications.*, pet_applications.*")
-    .joins(:pet_applications)
-    .where("pet_applications.application_id = #{appl_id}")
-  end
-
-
-
   def update_status
     if description == "" || pets.empty?
       update({status: 'In progress'})
     elsif description.length > 1 && !pets.empty?
       update({status: 'Pending'})
+    end
+  end
+
+  def approval
+    if pet_applications.all? {|pet_app| pet_app.pet_app_status == true}
+      update({status: 'Approved'})
+    elsif pet_applications.any? {|pet_app| pet_app.pet_app_status == false}
+      update({status: 'Rejected'})
     end
   end
 
